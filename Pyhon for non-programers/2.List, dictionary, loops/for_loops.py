ffef687b3d61da1ba297d3a18e60d8acc3372346
Vedran Karl <vedrankarl@Vedrans-Laptop.local>
2025-11-21 21:09:02 -0700

    Committer: Vedran Karl <vedrankarl@Vedrans-Laptop.local>

---
+++ Pyhon for non-programers/2.List, dictionary, loops/for_loops.py
@@ -0,0 +1,9 @@
+fav_movies = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "Pulp Fiction"]
+
+for number in range(1000000):
+    print(number)
+for movie in fav_movies:
+    print(movie)
+
+for pair in range(2,42, 2):
+    print(pair)
---
+++ Pyhon for non-programers/2.List, dictionary, loops/lists.py
@@ -0,0 +1,21 @@
+fav_movies = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "Pulp Fiction"]
+
+fav_movies.append('The Batman')
+
+print(fav_movies)
+
+fav_movies.insert(0, 'Beatlejuice')
+
+print(fav_movies)
+
+del(fav_movies[2])
+
+print(fav_movies)
+
+
+
+
+
+print(fav_movies.count("The Godfather"))
+
+print(len(fav_movies))

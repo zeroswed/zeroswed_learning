4eb3e66a91ea94429041e0cb0783315c1e3b69f4
Karl <vedran.karl@riotinto.com>
2025-11-23 08:58:29 -0700

    more_lessons

---
+++ Python Intermediate/1.Classes and OOP/classes.py
@@ -0,0 +1,16 @@
+class Dog:
+    info = 'Dog is a men best friend'
+    x = 4
+    y = 6
+
+print(Dog.info)
+
+
+#challenge
+
+class Network_room:
+    info = 'Network room is the room where network equipment and people'
+    inventory = ['monitors', 'tv','mouse','pc','desk','chair','whiteboard']
+    personel = ['Neil','Vedran']
+
+print(Network_room.personel[1])

9130e660c61261701aab47c5e3d4506f41b4f215
Karl <vedran.karl@riotinto.com>
2025-11-25 17:02:52 -0700

    merge

--- Python Intermediate/1.Classes and OOP/instances.py
+++ Python Intermediate/1.Classes and OOP/instances.py
@@ -3,14 +3,18 @@ import random
 class Dog:
     info = "Dog is a mens' best friend"
 
-    def __init__(self, name='Rex', age=2):
-#        print('I am alive!')
-#        print(random.randint(54, 68))
+    def __init__(self, name='Rex', age=2, breed='Labrador'):
+        print('I am alive!')
+        print(random.randint(54, 68))
         self.name = name
         self.age = age
         
 
 
-Dog1 = Dog('Brent', 4)
-print(Dog1.name,'and he is', Dog1.age)
+#Dog1 = Dog('Brent', 4)
+Dog2 = Dog('Ken', 9)
 
+#print(Dog)
+#print(Dog1.name,'and he is', Dog1.age)
+
+print(Dog2.name,'and he is', Dog2.age)

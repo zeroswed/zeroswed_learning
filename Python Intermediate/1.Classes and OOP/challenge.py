464c7b48cf25235669d3dbaa467d60edfe372bdc
Vedran Karl <vedrankarl@Vedrans-Laptop.local>
2025-11-27 20:22:24 -0700

    s

---
+++ Python Intermediate/1.Classes and OOP/challenge.py
@@ -0,0 +1,15 @@
+class Student:
+
+    scores = [65,75,85,95]
+    count = 0
+
+    def average_score(self):    
+        for score in self.scores:
+            self.count += 1
+        return sum(self.scores) / self.count
+
+print(Student().average_score())
+  
+
+
+
--- Python Intermediate/1.Classes and OOP/instances.py
+++ Python Intermediate/1.Classes and OOP/instances.py
@@ -5,15 +5,9 @@ import random
 class Dog():
     info = "Dog is a mens' best friend"
 
-<<<<<<< HEAD
     def __init__(self, name='Rex', age=2, breed='Labrador'):
         print('I am alive!')
         print(random.randint(54, 68))
-=======
-    def __init__(self, name):
-        print('A dog is created!')
-        self.lucky_number = random.randint(1,10)
->>>>>>> 5d5700a157ff82b39316367d0d3dc99e44f3b408
         self.name = name
         
 
@@ -25,7 +19,6 @@ dog1 = Dog('Fido')
         
 
 
-<<<<<<< HEAD
 #Dog1 = Dog('Brent', 4)
 Dog2 = Dog('Ken', 9)
 
@@ -33,5 +26,3 @@ Dog2 = Dog('Ken', 9)
 #print(Dog1.name,'and he is', Dog1.age)
 
 print(Dog2.name,'and he is', Dog2.age)
-=======
->>>>>>> 5d5700a157ff82b39316367d0d3dc99e44f3b408
